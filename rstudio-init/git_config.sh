6ce25c69dc69319cb931249cacaaf304bc77c786
Derek Powell <derekpowll@gmail.com>
2018-02-25 19:41:21 -0800

    fixed bug in git_Config.sh

--- rstudio-init/git_config.sh
+++ rstudio-init/git_config.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/with-contenv bash
 
 GIT_USER=${GIT_USER:=none}
-GIT_EMAIL=${GIT_USER:=none}
+GIT_EMAIL=${GIT_EMAIL:=none}
 
 if [ "$GIT_USER" != none ]; then
 	git config --global user.name $GIT_USER
